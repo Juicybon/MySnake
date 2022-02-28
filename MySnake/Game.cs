2b161d0cde613c836e2374870dc05a96a9d05721
Semyon Godyaev <godiaevsemyon@gmail.com>
2022-02-28 22:32:27 +0300

    Добавьте файлы проекта.

---
+++ MySnake/Game.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnake
+{
+    internal class Game
+    {
+        //размеры поля
+        static int height = 30;
+        static int width = 80;
+        static Walls walls;
+
+        static void Main()
+        {
+            Console.SetWindowSize(width + 1, height + 1);
+            Console.SetBufferSize(width + 1, height + 1);
+            Console.CursorVisible = false;
+            walls = new Walls(width, height, '#');
+            do { } while (true); //для проверки поля
+        }
+
+        struct Point
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public char ch { get; set; }
+
+            public static implicit operator Point((int, int, char) value) =>
+                new Point { x = value.Item1, y = value.Item2, ch = value.Item3 };//посмотреть снова
+            public void Draw()
+            {
+                DrawPoint(ch);
+            }
+            public void Clear()
+            {
+                DrawPoint(' ');
+            }
+            private void DrawPoint(char _ch)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(_ch);
+            }
+        }
+
+        class Walls
+        {
+            private char ch;
+            private List<Point> wall = new List<Point>();
+
+            public Walls(int x, int y, char ch)
+            {
+                this.ch = ch;
+                DrawHorizontalWall(x, 0);
+                DrawHorizontalWall(x + 1, y);
+                DrawVerticalWall(0, y);
+                DrawVerticalWall(x, y);
+            }
+
+            private void DrawHorizontalWall(int x, int y)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    Point p = (i, y, ch);
+                    p.Draw();
+                    wall.Add(p);
+                }
+            }
+            private void DrawVerticalWall(int x, int y)
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    Point p = (x, i, ch);
+                    p.Draw();
+                    wall.Add(p);
+                }
+            }
+        }
+
+    }
+}
