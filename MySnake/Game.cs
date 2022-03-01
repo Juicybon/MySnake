b33c99aac66bf48add1065456201c793aa2ffe11
Semyon Godyaev <godiaevsemyon@gmail.com>
2022-03-01 16:47:29 +0300

    Добавил комментарии, добавил класс змейки, добавил функцию первоначального состояния и создания Еды

--- MySnake/Game.cs
+++ MySnake/Game.cs
@@ -19,9 +19,14 @@ namespace MySnake
             Console.SetBufferSize(width + 1, height + 1);
             Console.CursorVisible = false;
             walls = new Walls(width, height, '#');
+            Snake mySnake = new Snake();
+            ElementaryStage(mySnake);
             do { } while (true); //для проверки поля
         }
 
+        /// <summary>
+        /// Структура рисования символа по координатам
+        /// </summary>
         struct Point
         {
             public int x { get; set; }
@@ -45,10 +50,14 @@ namespace MySnake
             }
         }
 
+        /// <summary>
+        /// Стена
+        /// </summary>
         class Walls
         {
             private char ch;
             private List<Point> wall = new List<Point>();
+
 
             public Walls(int x, int y, char ch)
             {
@@ -78,6 +87,61 @@ namespace MySnake
                 }
             }
         }
+
+        /// <summary>
+        /// Змейка
+        /// </summary>
+        public class Snake
+        {
+            public bool isSnakeAlive;
+            public int length = 3;
+            public int x = 42, y = 15;
+        }
+
+        /// <summary>
+        /// Начальное состояние игры
+        /// </summary>
+        public static void ElementaryStage(Snake mySnake)
+        {
+            mySnake.isSnakeAlive = true;
+            Point mySnakeHead = (mySnake.x, mySnake.y, '@');
+            mySnakeHead.Draw();
+            List<Point> mySnakeBody = new List<Point>();
+            for (int i = 1; i <= mySnake.length; i++)
+            {
+                mySnakeBody.Add((mySnake.x - i, mySnake.y, '0'));
+            }
+            foreach (Point row in mySnakeBody)
+            {
+                row.Draw();
+            }
+            CreateFood();
+        }
+
+        /// <summary>
+        /// Создание еды
+        /// </summary>
+        public static void CreateFood()
+        {
+            Random rnd = new Random();
+            int xFood;
+            int yFood;
+            bool isFoodCreated = false;
+            while (!isFoodCreated)
+            {
+                xFood = rnd.Next(2, 79);
+                yFood = rnd.Next(2, 29);
+                Console.SetCursorPosition(xFood, yFood);
+                //if () проверка на координаты змеи
+                //{
+                //    isFoodCreated = true;
+                //    Point food = (xFood, yFood, 'O');
+                //    food.Draw();
+                //}
+            }
+            
+        }
+
 
     }
 }
